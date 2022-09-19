ddcb21233833be3f18241b2adfd41a5ac3905919
VankProgrammingAndeDesign <vankerkvoorderyan@gmail.com>
2022-09-19 11:30:48 -0500

    Completed creating basic db operations

--- dbConnect.py
+++ dbConnect.py
@@ -10,7 +10,6 @@ db = client.AutotaskRPA
 ticketsDB = db.tickets
 partsDB = db.parts
 
-
 #Gets Part list from Part database based on ticket's device
 def getPartsList(ticketNum):
     ticket = ticketsDB.find_one({'Ticket Number':ticketNum},{'Title':1, '_id':0})
@@ -18,26 +17,30 @@ def getPartsList(ticketNum):
     print(deviceType)
     parts = partsDB.find_one({'models':deviceType},{'_id':0,'models':0})
 
+
     models = parts.get("MB")
     mobo = models.get(deviceType) 
     parts["Motherboard"] = parts.pop("MB")
     parts.update({"Motherboard": mobo})
-
-    for part in parts:
-        print(part + ":       "+ parts.get(part))
+    return parts
 
 
-#Returns ticket dictionary
+#Returns ticket dictionary(usually fpr ticket creation)
 def getTicketInfo(ticketNum):
     ticketInfo = ticketsDB.find_one({'Ticket Number':ticketNum},{'_id':0})
+
+    #Adds parts to ticketInfo
+    partsList=getPartsList(ticketNum)
+    ticketInfo["partsList"]=partsList
+
     return(ticketInfo)
-    
-    
-    
-ticketTest="T20220908.0644"
-test1 = Ticket(getTicketInfo(ticketTest))
 
-print(test1.deviceSN)
 
-      
+searchEntry = True
+while(searchEntry):  
+    searchEntry=input("Enter your ticket: ")
+    #print(getTicketInfo(searchEntry))
+    test1 = Ticket(getTicketInfo(searchEntry))
+    test1.printTicket()
+
 
--- ticket.py
+++ ticket.py
@@ -2,5 +2,18 @@ class Ticket:
   def __init__(self,ticketInfo):
     self.ticketNum = ticketInfo["Ticket Number"]
     self.deviceSN = ticketInfo['Installed Asset Serial Number']
-    
-    print(self.ticketNum + " Serial Number is:  "+ self.deviceSN)
+    self.status = ticketInfo["Status"]
+    self.school = ticketInfo["Client"]
+    self.model = ticketInfo["Title"]
+    self.notes = ticketInfo["Description"]
+    self.parts = ticketInfo["partsList"]
+
+  #
+  def printTicket(self):
+    print("Ticket Number: "+ self.ticketNum)
+    print("Status:  "+ self.status)
+    print("School:  "+ self.school)
+    print("Model: "+ self.model)
+    print("Notes: "+ self.notes)
+    print("Serial Number: "+ self.deviceSN)
+    #print(self.parts)
